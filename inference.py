9ab7287886cce4b72975e1d44d4504a35a6e0b47
Raúl Coterillo <raulcote98@gmail.com>
2022-12-27 16:53:29 +0100

    started implementation

--- inference.py
+++ inference.py
@@ -1,32 +1,15 @@
-import rasterio
+
+
+
+from data import ImageDataset
+from network import PredictionModel
+
 import numpy as np
 import pandas as pd
 
+from pytorch_lightning import Trainer
+import torch
 
-def load_image(image_path, remove_negs=True, normalization=False):
-    # Load the image as a numpy array
-    img = rasterio.open(image_path)
-    concat_list = []
-    if img.count == 5: # 1=B, 2=G, 3=R, 4=RE, 5=NIR
-        channels_list = [1,2,3,4,5]
-    elif img.count == 10: # 1=B, 3=G, 5=R, 7=RE, 10=NIR
-        channels_list = [1,3,5,7,10]
-    else:
-        raise Exception("Unexpected number of channels in image %s" %image_path)
-    for i in channels_list:
-        ch_image = img.read(i)
-        if remove_negs:
-            ch_image[ch_image < 0] = 0.0
-        concat_list.append(np.expand_dims(ch_image, -1))
-    image = np.concatenate(concat_list, axis=-1).astype(float) # float 0-1
-    if normalization:
-        max_pixel_value = np.max(image, axis=2)
-        max_pixel_value = np.repeat(max_pixel_value[:, :, np.newaxis], 5, axis=2)
-        image = np.divide(image, max_pixel_value)
-        image = image[:, :, 0:4]
-        image[np.isnan(image)] = 0.0
-
-    return image
 
 def predict(image):
     import random
@@ -34,12 +17,24 @@ def predict(image):
 
 
 if __name__=="__main__":
+
+    USE_FLAGS = True
+
     test_df = pd.read_csv("test.csv")
+    test_ds = ImageDataset(dataset=test_df, image_shape=[110,330], use_flags=USE_FLAGS)
+
+    model_file = "save/best.ckpt" if USE_FLAGS else "save/best_no_flags.ckpt"
+
+    model, trainer = PredictionModel.load_from_checkpoint(model_file), Trainer()
+    predictions = trainer.predict(model, dataloaders=test_ds)
+
     for image_path in test_df["PLOT_FILE"]:
         image = load_image(image_path=image_path, remove_negs=True, normalization=True)
         prediction = predict(image=image)
         test_df.loc[test_df.PLOT_FILE == image_path, ['DISEASE1', 'DISEASE2', 'DISEASE3']] = prediction
-    participant = "John_Smith"
+    
+    
+    participant = "Aprende_Máquina"
     version = "v1"
     results_filename = '-'.join([participant, 'test_results', version])
     test_df.to_csv(results_filename, index=False)
